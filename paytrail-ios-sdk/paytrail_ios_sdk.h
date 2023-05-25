e426355791add36ea88002569a9c16403665f98c
shiyuan <shiyuan.wang@qvik.fi>
2023-05-25 13:04:03 +0300

    Setup project

---
+++ paytrail-ios-sdk/paytrail_ios_sdk.h
@@ -0,0 +1,18 @@
+//
+//  paytrail_ios_sdk.h
+//  paytrail-ios-sdk
+//
+//  Created by shiyuan on 25.5.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for paytrail_ios_sdk.
+FOUNDATION_EXPORT double paytrail_ios_sdkVersionNumber;
+
+//! Project version string for paytrail_ios_sdk.
+FOUNDATION_EXPORT const unsigned char paytrail_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <paytrail_ios_sdk/PublicHeader.h>
+
+
