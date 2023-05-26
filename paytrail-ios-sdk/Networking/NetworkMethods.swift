4f8f7f4688d597082fa7c5553a3ae48f2f87f4fd
shiyuan <shiyuan.wang@qvik.fi>
2023-05-26 12:45:29 +0300

    Add network layer

---
+++ paytrail-ios-sdk/Networking/NetworkMethods.swift
@@ -0,0 +1,43 @@
+//
+//  NetworkMethods.swift
+//  paytrail-ios-sdk
+//
+//  Created by shiyuan on 25.5.2023.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
+
+protocol DataRequest {
+    associatedtype Response
+    var url: String { get }
+    var method: HTTPMethod { get }
+    var headers: [String : String] { get }
+    var queryItems: [String : String] { get }
+    
+    func decode(_ data: Data) throws -> Response
+}
+
+extension DataRequest where Response: Decodable {
+    func decode(_ data: Data) throws -> Response {
+        let decoder = JSONDecoder()
+        return try decoder.decode(Response.self, from: data)
+    }
+}
+
+extension DataRequest {
+    var headers: [String : String] {
+        [:]
+    }
+    
+    var queryItems: [String : String] {
+        [:]
+    }
+}
---
+++ paytrail-ios-sdk/PaytrailAPIs/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  paytrail-ios-sdk
+//
+//  Created by shiyuan on 26.5.2023.
+//
+
+import Foundation
+
+public final class Test {
+    static func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
